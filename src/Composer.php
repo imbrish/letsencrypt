c4c5131e297fe6e00a22aea842863eafa71e7b19
Paul <bmbaa1@gmail.com>
2017-01-26 14:53:55 +0100

    Remove 'post-root-package-install' script

--- src/Composer.php
+++ src/Composer.php
@@ -7,19 +7,6 @@ use Composer\Script\Event;
 class Composer
 {
     /**
-     * Create config file based on example.
-     *
-     * @param  \Composer\Script\Event  $event
-     * @return void
-     */
-    public static function createConfig(Event $event)
-    {
-        require_once $event->getComposer()->getConfig()->get('vendor-dir') . '/autoload.php';
-
-        copy('config.yml.example', 'config.yml');
-    }
-
-    /**
      * Copy acme command.
      * It has to be put to /bin so that it is able to find vendor classes.
      *
