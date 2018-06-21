b8b68105e19d898d037b89a14232ebe8b16908c3
Paul <bmbaa1@gmail.com>
2018-06-21 15:12:19 +0200

    Detect changes in composer.json and ask to re-install dependencies

--- src/Composer.php
+++ src/Composer.php
@@ -7,16 +7,19 @@ use Composer\Script\Event;
 class Composer
 {
     /**
-     * Copy acme command.
-     * It has to be put to /bin so that it is able to find vendor classes.
+     * Post install/post update callback.
      *
      * @param  \Composer\Script\Event  $event
      * @return void
      */
-    public static function copyAcme(Event $event)
+    public static function handle(Event $event)
     {
         require_once $event->getComposer()->getConfig()->get('vendor-dir') . '/autoload.php';
 
+        // we need to put kelunik/acme-client executable to "bin" so that it is able to find composer's "autoload.php"
         copy('vendor/kelunik/acme-client/bin/acme', 'bin/acme');
+
+        // we will calculate hash of the "composer.json" so that we can easily check whether it was changed
+        file_put_contents('storage/composer.hash', md5(file_get_contents('composer.json')));
     }
 }
