8f7d60c45c4a8c5eac9cf80a2c51fbb7dd3b246c
Paul <bmbaa1@gmail.com>
2018-06-23 17:24:37 +0200

    Install certificates using UAPI if available

--- src/Command.php
+++ src/Command.php
@@ -22,7 +22,7 @@ class Command {
      * 
      * @var array
      */
-    public static $aliases = [];
+    public static $aliases;
 
     /**
      * Last executed command.
@@ -46,19 +46,26 @@ class Command {
     public static $output;
 
     /**
-     * Flat array of commands parts.
+     * Non escaped command.
+     * 
+     * @var string
+     */
+    protected $cmd;
+
+    /**
+     * Flat array of command parts.
      * 
      * @var array
      */
-    protected $parts = [];
+    protected $parts;
 
     /**
-     * Execute command.
+     * Execute command and return result code.
      *
      * @param string $cmd
      * @param array $args
      *
-     * @return mixed
+     * @return int
      */
     public static function exec($cmd, $args = [])
     {
@@ -75,17 +82,23 @@ class Command {
      */
     public function __construct($cmd, $args = [])
     {
-        // initialize command from alias or as given
+        // initialize command parts from alias or as given
         if (array_key_exists($cmd, static::$aliases)) {
             $cmd = (array) static::$aliases[$cmd];
         }
 
-        $this->parts = (array) $cmd;
+        $parts = array_merge((array) $cmd, $args);
 
-        // convert arguments into command parts
-        foreach ($args as $key => $value) {
+        $this->cmd = reset($parts);
+
+        // convert keyed parts into options and escape where necessary
+        foreach ($parts as $key => $value) {
+            if (strpos($value, ' ') !== false) {
+                $value = escapeshellarg($value);
+            }
+
             if (! is_int($key)) {
-                $this->parts[] = $key;
+                $value = $key . '=' . $value;
             }
 
             $this->parts[] = $value;
@@ -93,36 +106,106 @@ class Command {
     }
 
     /**
-     * Execute command.
+     * Execute command and return result code.
      *
-     * @return mixed
+     * @return int
      */
     public function __invoke()
     {
-        // escape command parts where necessary
-        $parts = array_map(function ($part) {
-            return strpos($part, ' ') === false ? $part : escapeshellarg($part);
-        }, $this->parts);
+        if ($this->cmd === PHP_BINARY) {
+            $method = 'handlePHP';
+        }
+        else if ($this->cmd === UAPI_BINARY) {
+            $method = 'handleUAPI';
+        }
+        else {
+            $method = 'handle';
+        }
 
-        // save and show executed command
-        static::$climate->comment(
-            static::$last = implode(' ', $parts)
+        $this->$method($this->parts);
+
+        return static::$result;
+    }
+
+    /**
+     * Store and display last command.
+     *
+     * @param string $command
+     *
+     * @return void
+     */
+    protected function lastCall($command)
+    {
+        static::$last = $command;
+
+        static::$climate->comment($command);
+    }
+
+    /**
+     * Store and display last result and output.
+     *
+     * @param int $result
+     * @param string $output
+     *
+     * @return void
+     */
+    protected function lastResult($result, $output)
+    {
+        static::$result = $result;
+
+        $output = preg_replace('/^[\t ]*[\n\r]*/m', '', trim($output));
+
+        static::$output = $output;
+
+        if ($output) {
+            static::$climate->out($output);
+        }
+    }
+
+    /**
+     * Handle generic command.
+     *
+     * @param array $parts
+     *
+     * @return void
+     */
+    protected function handle($parts)
+    {
+        $this->lastCall(
+            $command = implode(' ', $parts)
         );
+
+        exec($command, $output, $result);
+
+        $this->lastResult(
+            $result, implode(PHP_EOL, $output)
+        );
+    }
+
+    /**
+     * Handle PHP command.
+     *
+     * @param array $parts
+     *
+     * @return void
+     */
+    protected function handlePHP($parts)
+    {
+        $this->lastCall(implode(' ', $parts));
 
         // redirect PHP error log to fetch errors after execution
         $logPath = static::$config['storage'] . '/error.log';
 
-        if (in_array($this->parts[0], ['php', PHP_BINARY])) {
-            array_splice($parts, 1, 0, [
-                '-d', 'errorLog=' . escapeshellarg($logPath),
-            ]);
-        }
+        array_splice($parts, 1, 0, [
+            '-d',
+            'errorLog=' . escapeshellarg($logPath),
+        ]);
 
         // tunnel error output to the standard output
         $parts[] = '2>&1';
 
         // run command, store its output and return code
-        exec(implode(' ', $parts), $output, $code);
+        exec(implode(' ', $parts), $output, $result);
 
         // get and erase temporary error log, remove dates from the logged errors
         if (file_exists($logPath)) {
@@ -134,16 +217,50 @@ class Command {
             $errorLog = '';
         }
 
-        // save and show command output together with collected error logs
-        // remove leading whitespace from each line and empty lines
-        static::$output = rtrim(implode(PHP_EOL, $output)) . PHP_EOL . $errorLog;
-        static::$output = preg_replace('/^[\t ]*[\n\r]*/m', '', trim(static::$output));
+        // merge command output and collected error logs
+        $this->lastResult(
+            $result, implode(PHP_EOL, $output) . PHP_EOL . $errorLog
+        );
+    }
 
-        if (static::$output) {
-            static::$climate->out(static::$output);
+    /**
+     * Handle UAPI command.
+     *
+     * @param array $parts
+     *
+     * @return void
+     */
+    protected function handleUAPI($parts)
+    {
+        // add default arguments, when used as root we need to specify cPanel user
+        array_splice($parts, 1, 0, array_filter([
+            '--output=json',
+            posix_getuid() == 0 ? '--user=' . escapeshellarg(static::$config['cpanel']['user']) : null,
+        ]));
+
+        // obfuscate certificate data for cleaner output
+        $this->lastCall(
+            preg_replace('/(-----BEGIN\+[^ ]+)/', '***', implode(' ', $parts))
+        );
+
+        // run command and parse response to determine result code and output
+        $response = shell_exec(implode(' ', $parts));
+
+        if ($response = json_decode($response, true)) {
+            $status = isset($response['result']['status']) ? $response['result']['status'] : 0;
+
+            $messages = array_merge(
+                isset($response['result']['errors']) ? $response['result']['errors'] : [],
+                isset($response['result']['messages']) ? $response['result']['messages'] : []
+            );
+
+            $this->lastResult(
+                $status ? 0 : 255,
+                $messages ? implode(PHP_EOL, $messages) : 'The UAPI call failed for unknown reason.'
+            );
         }
-
-        // save and return last code
-        return static::$result = $code;
+        else {
+            $this->lastResult(255, 'The UAPI call did not return a valid response.');
+        }
     }
 }
